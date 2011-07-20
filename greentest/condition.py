a6d4336fe1292819a738c07144a5fab74064f592
Sumit Khanna <sumit@penguindreams.org>
2011-07-20 16:59:52 -0400

    Cleaned up tests and started first real GreenOven functional test.

--- greentest/condition.py
+++ greentest/condition.py
@@ -14,9 +14,32 @@ class AbstractSuccessCondition:
     
     return False
 
-class OKSuccessCondition(AbstractSuccessCondition):
+class StatusSuccessCondition(AbstractSuccessCondition):
   
-  description = '200 Success'
+  description = property(lambda self: 'Status {0} {1}'.format(self.code, self.status) )
+  code = -1
+  status = 'Unimplemented'
   
   def run_check(self,test : AbstractTest):
-    return test.resultStatus == http.client.OK
+    return test.resultStatus == self.code
+  
+class HeaderSuccessCondition(AbstractSuccessCondition):
+  
+  description = property(lambda self: 'Header {0} (min/max: {1}/{2}'.format(self.header,self.min,self.max))
+  header = None
+  min = 1
+  max = 1
+  
+  def run_check(self, test : AbstractTest):
+    
+    if self.header == None:
+      return False
+    
+    count = 0
+    for h in test.resultHeaders:
+      if h == self.header:
+        count += 1
+    return count > min and count < max
+  
+  
+  
