06261cf859ae33149263c67147f0789f40924a4c
Sumit Khanna <sumit@example.com>
2017-04-09 22:47:28 -0700

    Base implementation of full multi-database tests

--- docker-tests.rb
+++ docker-tests.rb
@@ -2,6 +2,7 @@
 
 require 'docker'
 require 'logger'
+require 'socket'
 
 $log = Logger.new(STDOUT)
 
@@ -18,175 +19,81 @@ def sense_containers
   }
 end
 
-def database_container(name, image, port, env)
+def wait_for_service(port)
+  begin
+    Timeout::timeout(5) do
+      begin
+        s = TCPSocket.new('localhost', port)
+        s.close
+        $log.info("Service accepting connections on #{port}")
+        return true
+      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+        sleep(2)
+        retry
+      end
+    end
+  rescue Timeout::Error
+    $log.info("Service not ready on #{port}. Retrying...")
+    return false
+  end
+end
+
+def create_container(name, image, dockerfile, port, env, link)
   {
    name => {
      'type' => 'docker',
      'image' => image,
+     'image_args' => ({
+       'dockerfile' => dockerfile
+     } if not dockerfile.nil?),
      'container_args' => {
        'Env' => env,
-       'ExposedPorts' => {"#{port}/tcp" => {}},
+       'ExposedPorts' => ({"#{port}/tcp" => {}} if not port.nil?),
        'HostConfig' => {
-         'PortBindings' => {
+         'Links' => (["bigsense-#{link}"] if not link.nil?),
+         'PortBindings' => ({
            "#{port}/tcp" => [{ 'HostPort' => port }]
-         }
-       }
+         } if not port.nil?)
+       }.reject{ |k,v| v.nil? }
      }
-   }
+   }.reject{ |k,v| v.nil? }
+ }
 end
 
-def container_config
-  {
-   'mysql' => {
-     'type' => 'docker',
-     'image' => 'mysql/mysql-server:5.7',
-     'container_args' => {
-       'Env' => [
-         'MYSQL_ROOT_PASSWORD=testroot',
-         'MYSQL_DATABASE=BigSense',
-         'MYSQL_USER=bigsense',
-         'MYSQL_PASSWORD=bigsense'
-       ],
-       'ExposedPorts' => {'3306/tcp' => {}},
-       'HostConfig' => {
-         'PortBindings' => {
-          '3306/tcp' => [{ 'HostPort' => '3306' }]
-         }
-       }
-     }
-   },
-   'postgres' => {
-     'type' => 'docker',
-     'image_args' => {
-       'dockerfile' => 'BigSensePostgres'
-     },
-     'container_args' => {
-       'Env' => [
-         'POSTGRES_DB=bigsense',
-         'POSTGRES_USER=bigsense_ddl',
-         'POSTGRES_PASSWORD=bigsense_ddl'
-       ],
-       'ExposedPorts' => {'5432/tcp' => {}},
-       'HostConfig' => {
-         'PortBindings' => {
-          '5432/tcp' => [{ 'HostPort' => '5432' }]
-         }
-       }
-     }
-   },
-   'mssql' => {
-     'type' => 'docker',
-     'image' => 'microsoft/mssql-server-linux',
-     'container_args' => {
-       'Env' => [
-         'ACCEPT_EULA=Y',
-         'SA_PASSWORD=B1gS3ns301'
-       ],
-      'ExposedPorts' => {'1433/tcp' => {}},
-      'HostConfig' => {
-        'PortBindings' => {
-         '1433/tcp' => [{ 'HostPort' => '1433' }]
-        }
-      }
-     }
-   },
-   'mssql-client' => {
-     'type' => 'docker',
-     'image_args' => {
-       'dockerfile' => 'MsSqlClient'
-     },
-     'container_args' => {
-       'Env' => [
-         'DATABASE=BigSense',
-         'SA_PASSWORD=B1gS3ns301',
-         'DATABASE=BigSense',
-         'DB_USER=bigsense',
-         'DB_PASS=B1gs3nse400'
-       ],
-       'HostConfig' => {
-         'Links' => ["bigsense-mssql"]
-       }
-     }
-   },
-   'tomcat-mysql' => {
-     'type' => 'docker',
-     'image' => 'bigsense.io/bigsense',
-     'container_args' => {
-       'Env' => [
-         "DBMS=mysql",
-         "DB_HOSTNAME=bigsense-mysql",
-         "DB_DATABASE=BigSense",
-         "DB_PORT=3306",
-         "DB_USER=bigsense",
-         "DB_PASS=bigsense",
-         "DBO_USER=root",
-         "DBO_PASS=testroot",
-         "SECURITY_MANAGER=Disabled",
-         "SERVER=tomcat",
-         "HTTP_PORT=9090"
-       ],
-       'ExposedPorts' => {'9090/tcp' => {}},
-       'HostConfig' => {
-         'Links' => ["bigsense-mysql"],
-         'PortBindings' => {
-          '9090/tcp' => [{ 'HostPort' => '9090' }]
-         }
-       }
-     }
-   },
-   'tomcat-postgres' => {
-     'type' => 'docker',
-     'image' => 'bigsense.io/bigsense',
-     'container_args' => {
-       'Env' => [
-         "DBMS=pgsql",
-         "DB_HOSTNAME=bigsense-postgres",
-         "DB_DATABASE=bigsense",
-         "DB_PORT=5432",
-         "DB_USER=bigsense",
-         "DB_PASS=bigsense",
-         "DBO_USER=bigsense_ddl",
-         "DBO_PASS=bigsense_ddl",
-         "SECURITY_MANAGER=Disabled",
-         "SERVER=tomcat",
-         "HTTP_PORT=9091"
-       ],
-       'ExposedPorts' => {'9091/tcp' => {}},
-       'HostConfig' => {
-         'Links' => ["bigsense-postgres"],
-         'PortBindings' => {
-          '9091/tcp' => [{ 'HostPort' => '9091' }]
-         }
-       }
-     }
-   },
-   'tomcat-mssql' => {
-     'type' => 'docker',
-     'image' => 'bigsense.io/bigsense',
-     'container_args' => {
-       'Env' => [
-         "DBMS=mssql",
-         "DB_HOSTNAME=bigsense-mssql",
-         "DB_DATABASE=bigsense",
-         "DB_PORT=1433",
-         "DB_USER=bigsense",
-         "DB_PASS=bigsense",
-         "DBO_USER=bigsense_ddl",
-         "DBO_PASS=bigsense_ddl",
-         "SECURITY_MANAGER=Disabled",
-         "SERVER=tomcat",
-         "HTTP_PORT=9091"
-       ],
-       'ExposedPorts' => {'9092/tcp' => {}},
-       'HostConfig' => {
-         'Links' => ["bigsense-mssql"],
-         'PortBindings' => {
-          '9092/tcp' => [{ 'HostPort' => '9092' }]
-         }
-       }
-     }
-   }
-  }
+def db_containers()
+  create_container('db-mysql', 'mysql/mysql-server:5.7', nil, '3306', [
+    'MYSQL_ROOT_PASSWORD=testroot', 'MYSQL_DATABASE=BigSense', 'MYSQL_USER=bigsense', 'MYSQL_PASSWORD=bigsense'
+  ], nil).merge(
+  create_container('db-postgres', nil, 'BigSensePostgres', '5432', [
+    'POSTGRES_DB=bigsense', 'POSTGRES_USER=bigsense_ddl', 'POSTGRES_PASSWORD=bigsense_ddl'
+  ], nil).merge(
+  create_container('db-mssql', 'microsoft/mssql-server-linux', nil, '1433', [
+    'ACCEPT_EULA=Y', 'SA_PASSWORD=B1gS3ns301'
+  ], nil).merge(
+  create_container('client-mssql', nil, 'MsSqlClient', nil, [
+    'DATABASE=BigSense', 'SA_PASSWORD=B1gS3ns301', 'DATABASE=BigSense', 'DB_USER=bigsense_ddl',
+    'DB_PASSWORD=bigsense_ddl'
+  ], 'db-mssql')
+  )))
+end
+
+def bs_containers()
+  create_container('tomcat-mysql', 'bigsense.io/bigsense', nil, '9090', [
+    "DBMS=mysql", "DB_HOSTNAME=bigsense-db-mysql", "DB_DATABASE=BigSense", "DB_PORT=3306",
+    "DB_USER=bigsense", "DB_PASS=bigsense", "DBO_USER=root", "DBO_PASS=testroot",
+    "SECURITY_MANAGER=Disabled", "SERVER=tomcat", "HTTP_PORT=9090"
+  ], 'db-mysql').merge(
+  create_container('tomcat-postgres', 'bigsense.io/bigsense', nil, '9091',  [
+    "DBMS=pgsql", "DB_HOSTNAME=bigsense-db-postgres", "DB_DATABASE=bigsense", "DB_PORT=5432",
+    "DB_USER=bigsense", "DB_PASS=bigsense", "DBO_USER=bigsense_ddl", "DBO_PASS=bigsense_ddl",
+    "SECURITY_MANAGER=Disabled", "SERVER=tomcat", "HTTP_PORT=9091"
+  ], 'db-postgres').merge(
+  create_container('tomcat-mssql', 'bigsense.io/bigsense', nil, '9092',  [
+    "DBMS=mssql", "DB_HOSTNAME=bigsense-db-mssql", "DB_DATABASE=bigsense", "DB_PORT=1433",
+    "DB_USER=bigsense", "DB_PASS=bigsense", "DBO_USER=SA", "DBO_PASS=B1gS3ns301",
+    "SECURITY_MANAGER=Disabled", "SERVER=tomcat", "HTTP_PORT=9092"
+  ], 'db-mssql')
+  ))
 end
 
 def existing_containers
@@ -197,8 +104,8 @@ end
 #  existing_containers.find { |c| c.info['Names'].any? { |n| } }
 #end
 
-def create_containers
-  container_config.map { |name, params|
+def create_containers(containers)
+  containers.map { |name, params|
     # for all bigsense containers that don't exist yet
     if not existing_containers.any? { |e| e.info['Names'].any? { |n| n.split('-')[1] == name } }
       case params['type']
@@ -216,7 +123,7 @@ def create_containers
         Docker::Container.create(
           {
             'Image' => image,
-            'name' => 'bigsense-%s' %[name]
+          'name' => 'bigsense-%s' %[name]
           }.merge(params['container_args'])
         ).start()
         $log.info('bigsense-%s container started' %[name])
@@ -237,4 +144,18 @@ def clean_containers
 end
 
 clean_containers()
-create_containers()
+create_containers(db_containers)
+wait_for_service(3306)
+wait_for_service(5432)
+wait_for_service(1433)
+create_containers(bs_containers)
+wait_for_service(9090)
+wait_for_service(9091)
+wait_for_service(9092)
+bs_mysql = system('./bst.py BigSenseMasterTestSet -s localhost -p 9090')
+bs_pgsql = system('./bst.py BigSenseMasterTestSet -s localhost -p 9091')
+bs_mssql = system('./bst.py BigSenseMasterTestSet -s localhost -p 9092')
+
+puts("MySQL Tests: #{bs_mysql}")
+puts("Postgres Tests: #{bs_pgsql}")
+puts("MS SQL Tests: #{bs_mssql}")
--- dockerfiles/mssql-bigsense-init.sh
+++ dockerfiles/mssql-bigsense-init.sh
@@ -3,7 +3,7 @@
 echo "Preparing BigSense Init >>"
 envsubst  < /opt/bigsense/mssql-db-init.sql > /tmp/init.sql
 cat /tmp/init.sql
-while ! /opt/mssql-tools/bin/sqlcmd -S bigsense-mssql -U SA -P $SA_PASSWORD -i /tmp/init.sql
+while ! /opt/mssql-tools/bin/sqlcmd -S bigsense-db-mssql -U SA -P $SA_PASSWORD -i /tmp/init.sql
 do
   echo "SQL Error Retrying in 5 sec"
   sleep 5
