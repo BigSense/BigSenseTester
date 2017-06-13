b26de3b5209ae8243ed137ccd8f3d785d20924fe
Sumit Khanna <sumit@example.com>
2017-06-13 14:51:57 -0700

    Updated json generator for empty fields

--- greentest/generator.py
+++ greentest/generator.py
@@ -6,6 +6,7 @@ Created on Jul 18, 2011
 from xml.dom.minidom import Document
 import time
 import base64
+import json
 
 
 class AbstractGenerator:
@@ -21,6 +22,13 @@ class AbstractGenerator:
   def generate_data(self):
     return "Unimplemented"
 
+  def _loc_not_empty(self, ele):
+    return ele in self.location and self.location[ele] != ''
+
+  def _location(self, ele):
+    return str(self.location[ele]) if ele in self.location else ''
+
+
 class RegistrationDataGenerator(AbstractGenerator):
 
   def __init__(self):
@@ -34,7 +42,7 @@ class RegistrationDataGenerator(AbstractGenerator):
     return doc.toprettyxml(indent=' ')
 
 
-class XMLDataGenerator(AbstractGenerator):
+class JSONDataGenerator(AbstractGenerator):
 
   def __init__(self):
     AbstractGenerator.__init__(self)
@@ -43,11 +51,56 @@ class XMLDataGenerator(AbstractGenerator):
     self.name = "FunctionalTester"
     self.location = None
 
-  def _loc_not_empty(self, ele):
-    return ele in self.location and self.location[ele] != ''
+  def generate_data(self):
 
-  def _location(self, ele):
-    return str(self.location[ele]) if ele in self.location else ''
+    packages = []
+    for i in range(int(self.numPackages)):
+        data = {'id': self.name,
+                'timestamp': '{0}'.format(round(time.time()* 1000)) }
+
+        if self.location:
+          data['gps'] = {}
+
+          if self._loc_not_empty('longitude') or self._loc_not_empty('latitude') or self._loc_not_empty('altitude'):
+            data['gps']['location'] = {}
+            data['gps']['location']['longitude'] = float(self._location('longitude'))
+            data['gps']['location']['latitude'] = float(self._location('latitude'))
+            data['gps']['location']['altitude'] = float(self._location('altitude'))
+
+          if self._loc_not_empty('speed') or self._loc_not_empty('track') or self._loc_not_empty('climb'):
+            data['gps']['delta'] = {}
+            data['gps']['delta']['speed'] = float(self._location('speed'))
+            data['gps']['delta']['track'] = float(self._location('track'))
+            data['gps']['delta']['climb'] = float(self._location('climb'))
+
+          if self._loc_not_empty('longitude_error') or self._loc_not_empty('latitude_error') or self._loc_not_empty('altitude_error') or self._loc_not_empty('speed_error') or self._loc_not_empty('track_error') or self._loc_not_empty('climb_error'):
+            data['gps']['accuracy'] = {}
+            data['gps']['accuracy']['longitude_error'] = float(self._location('longitude_error'))
+            data['gps']['accuracy']['latitude_error'] = float(self._location('latitude_error'))
+            data['gps']['accuracy']['altitude_error'] = float(self._location('altitude_error'))
+            data['gps']['accuracy']['speed_error'] = float(self._location('speed_error'))
+            data['gps']['accuracy']['climb_error'] = float(self._location('climb_error'))
+            data['gps']['accuracy']['track_error'] = float(self._location('track_error'))
+
+        data['sensors'] = []
+        for s in self.sensors:
+            data['sensors'].append({'id': s.id,
+                                    'type': s.type,
+                                    'units': s.units,
+                                    'data': s.data})
+        packages.append(data)
+
+    return json.dumps(packages)
+
+
+class XMLDataGenerator(AbstractGenerator):
+
+  def __init__(self):
+    AbstractGenerator.__init__(self)
+    self.numPackages = 1
+    self.sensors = []
+    self.name = "FunctionalTester"
+    self.location = None
 
   def generate_data(self):
     """Creates Sense XML. Location None or {x, y, accuracy, altitude}"""
