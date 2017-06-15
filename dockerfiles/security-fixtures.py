440dd7cd907657c49d69ea77f4fc3ce64c0ba1a9
Sumit Khanna <sumit@example.com>
2017-06-15 14:08:42 -0700

    Passing JSON/XML base location tests

--- dockerfiles/security-fixtures.py
+++ dockerfiles/security-fixtures.py
@@ -27,7 +27,6 @@ with open('sig-fixture.sql', 'r') as template:
 with open(RELAY_TMP, 'w') as out:
     for r in relays:
         out.write(sql.replace('%relay%', r))
-    for r in relays:
         for f in ['XML', 'JSON']:
             out.write(sql.replace('%relay%', '{}{}'.format(r, f)))
 
